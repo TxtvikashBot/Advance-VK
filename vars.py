3563468f282b8b251cd42f836594857fa64c9be2
Vikash kumar <bindukumar480@gmail.com>
2025-06-27 19:34:39 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,9 +1,9 @@
 from os import getenv
 
-api_id    = int(getenv("api_id", ""))
-api_hash  = getenv("api_hash", "")
+api_id    = int(getenv("api_id", "29747831"))
+api_hash  = getenv("api_hash", "5a3842719ef9614b1ff57615a338e128")
 bot_token = getenv("bot_token", "")
 pwtoken = getenv("pwtoken", "")
 CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
-AUTH = ""
-#Bot Username = ""
+AUTH = "1304973931"
+#Bot Username = "@Technicalboyv"

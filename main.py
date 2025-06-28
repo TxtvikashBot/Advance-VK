b5fe1f1277d2f9d462527bd2fc4a7dc6c1afe85c
Vikash kumar <bindukumar480@gmail.com>
2025-06-28 12:42:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -138,7 +138,7 @@ async def upload(bot: Client, m: Message):
 
     try:
         for i in range(count - 1, len(links)):
-
+            name1 = links[i][0].strip()
             V = links[i][1].replace("file/d/","uc?export=download&id=").replace("www.youtube-nocookie.com/embed", "youtu.be").replace("?modestbranding=1", "").replace("/view?usp=sharing","") # .replace("mpd","m3u8")
             url = "https://" + V
 

b35f2ab19653aedcf5959bf0c9c74ee645458e16
Vikash kumar <bindukumar480@gmail.com>
2025-06-28 15:14:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -179,15 +179,15 @@ async def upload(bot: Client, m: Message):
                    "https://api.classplusapp.com/cams/uploader/video/jw-signed-url",
                    headers=headers,
                    params=params
-              )
-              if response.status_code == 200 and "url" in response.json():
-                  url = response.json()["url"]
-              else:
-                  await m.reply_text(f"❌ Failed to fetch signed URL:\n\n{response.text}")
-                  return
-          except Exception as e:
-              await m.reply_text(f"❌ Error while signing URL:\n{str(e)}")
-              return
+                )
+                if response.status_code == 200 and "url" in response.json():
+                    url = response.json()["url"]
+                else:
+                    await m.reply_text(f"❌ Failed to fetch signed URL:\n\n{response.text}")
+                    return
+            except Exception as e:
+                await m.reply_text(f"❌ Error while signing URL:\n{str(e)}")
+                return
             elif 'videos.classplusapp' in url:
              url = requests.get(f'https://api.classplusapp.com/cams/uploader/video/jw-signed-url?url={url}', headers={'x-access-token': 'eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpZCI6MzgzNjkyMTIsIm9yZ0lkIjoyNjA1LCJ0eXBlIjoxLCJtb2JpbGUiOiI5MTcwODI3NzQyODkiLCJuYW1lIjoiQWNlIiwiZW1haWwiOm51bGwsImlzRmlyc3RMb2dpbiI6dHJ1ZSwiZGVmYXVsdExhbmd1YWdlIjpudWxsLCJjb3VudHJ5Q29kZSI6IklOIiwiaXNJbnRlcm5hdGlvbmFsIjowLCJpYXQiOjE2NDMyODE4NzcsImV4cCI6MTY0Mzg4NjY3N30.hM33P2ai6ivdzxPPfm01LAd4JWv-vnrSxGXqvCirCSpUfhhofpeqyeHPxtstXwe0'}).json()['url']
 

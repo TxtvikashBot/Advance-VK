761fda350d18ff8316ffebcc0f019474b4e4509c
Vikash kumar <bindukumar480@gmail.com>
2025-06-29 19:48:53 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,5 +1,9 @@
+import os
+from os import environ
 
-BOT_TOKEN = "YOUR_BOT_TOKEN"
+API_ID = int(environ.get("API_ID", "29747831"))
+API_HASH = environ.get("API_HASH", "5a3842719ef9614b1ff57615a338e128")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 ADMIN_ID = 1304973931
 PRICE = 100
 DAYS = 7

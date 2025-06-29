681f3dd184f5a1579a722cf7039f84a2fc2f9307
Vikash kumar <bindukumar480@gmail.com>
2025-06-29 13:16:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import wget
 
 import core as helper
 from utils import progress_bar
-from vars import api_id, api_hash, bot_token
+from vars import API_ID, API_HASH, BOT_TOKEN
 from aiohttp import ClientSession
 from pyromod import listen
 from subprocess import getstatusoutput

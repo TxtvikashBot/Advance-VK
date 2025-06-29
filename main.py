427fa23165e252fa0453754e9b11f9c572d97527
Vikash kumar <bindukumar480@gmail.com>
2025-06-29 19:45:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ bot = Client(
     "bot",
     api_id=API_ID,
     api_hash=API_HASH,
-    bot_token=BOT_TOKEN)
+    bot_token=BOT_TOKEN
+)
 
 Clienbot = Client("warrior_txt_handler", bot_token=BOT_TOKEN)
 

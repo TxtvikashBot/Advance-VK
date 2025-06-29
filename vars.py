328c60c919207905cd8b87c7c975cf82e5a367e0
Vikash kumar <bindukumar480@gmail.com>
2025-06-29 16:41:55 +0530

    Add files via upload

--- vars.py
+++ vars.py
@@ -1,9 +1,6 @@
-from os import getenv
 
-api_id    = int(getenv("api_id", "29747831"))
-api_hash  = getenv("api_hash", "5a3842719ef9614b1ff57615a338e128")
-bot_token = getenv("bot_token", "")
-pwtoken = getenv("pwtoken", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002608152504"))
-AUTH = "1304973931"
-#Bot Username = "@Technicalboyv"
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+ADMIN_ID = 123456789
+PRICE = 100
+DAYS = 7
+WATERMARK_TAG = "_WarriorUploader"

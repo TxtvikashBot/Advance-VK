6cba1ac898c5e62c7764429ef221546862d11b20
Vikash kumar <bindukumar480@gmail.com>
2025-06-29 12:08:58 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -9,7 +9,7 @@ def index():
     return "✅ Bot is running on Render!"
 
 def run():
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
 
 def keep_alive():
